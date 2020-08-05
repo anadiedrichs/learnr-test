3cdfad3b1fbbd63eef3207510392382810b73bb8
Ana Diedrichs (yoga) <ana.diedrichs@frm.utn.edu.ar>
2020-08-04 23:07:56 -0300

    files created using bindertools

--- install.R
+++ install.R
@@ -1,2 +1,3 @@
-install.packages("rmarkdown")
-install.packages("learnr")
+install.packages(c('purrr', 'devtools'))
+purrr::walk2(.x = c("rmarkdown", "learnr", "knitr"), .y = c("2.3", "0.10.1", "1.29"), ~devtools::install_version(package = .x, version = .y))
+
